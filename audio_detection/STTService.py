4ea5ea5a2376df9b3084af0ffe1f994f6384e927
gabrielegiuli <gabriele.giuli18@imperial.ac.uk>
2021-09-25 16:36:58 +0200

    added audio upload

--- audio_detection/STTService.py
+++ audio_detection/STTService.py
@@ -2,25 +2,36 @@ import sys
 import speech_recognition
 import firebase_admin
 from firebase_admin import db
+from firebase_admin import storage
 from datetime import datetime
 
 # function for submitting help request
-def process_text(text, audio, dbRef):
+def process_text(text, audio, dbRef, bucket, uid):
     print("DECODED TEXT: " + text)
 
     # Look for keyword
     if text.find("send help") != -1:
         print("HELP REQUEST HAS BEEN SENT")
 
+        # Save audio file
+        with open("audio_file.wav", "wb") as file:
+            file.write(audio.get_wav_data())
+
+        blobName = datetime.now().strftime("AUDIO%d%b%Y%H%M%S") + '.wav'
+        blobPath = str(uid) + '/' + blobName
+        blob = bucket.blob(blobPath)
+        blob.upload_from_filename('audio_file.wav')
+
         # Post request in firebase RTDB
         dbRef.push({
             "timestamp": datetime.now().strftime("%d-%b-%Y (%H:%M:%S.%f)"),
             "type": "audio",
+            "resourceBucketLocation" : blobPath,
             "audioTranscript": text
         })
 
 # Crate listen function
-def listen(recognizer, dbRef):
+def listen(recognizer, dbRef, bucket, uid):
         try:
             with speech_recognition.Microphone() as mic:
 
@@ -31,7 +42,7 @@ def listen(recognizer, dbRef):
                 text = text.lower()
 
             # Process input text (MIGHT NEED TO RM FOR ASYNC)
-            process_text(text, audio, dbRef)
+            process_text(text, audio, dbRef, bucket, uid)
 
         except speech_recognition.UnknownValueError:
             recognizer = speech_recognition.Recognizer()
@@ -44,20 +55,24 @@ def main():
 
     # Connect to firebase database
     databaseURL = 'https://emergency-monitor-hz21-default-rtdb.europe-west1.firebasedatabase.app'
+    bucketURL = 'emergency-monitor-hz21.appspot.com'
+
     cred_obj = firebase_admin.credentials.Certificate('../auth_key/key.json')
     default_app = firebase_admin.initialize_app(cred_obj, {
-	   'databaseURL' : databaseURL
+	   'databaseURL' : databaseURL,
+       'storageBucket' : bucketURL
 	})
 
-    # Create database reference
+    # Create database reference and storage bucket
     ref = db.reference("/users/" + userID + '/emergencies')
+    bucket = storage.bucket()
 
     # Set up recognizer
     recognizer = speech_recognition.Recognizer()
 
     # Start event loop
     while True:
-        listen(recognizer, ref)
+        listen(recognizer, ref, bucket, userID)
 
 # Define entry point
 if __name__ == "__main__":
